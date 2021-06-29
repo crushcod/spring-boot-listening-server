a99bb200c8b5bb2fa3c88da749e442b747fdd724
Stefano Filippone <filippone.stefano@gmail.com>
2021-06-29 15:19:38 -0300

    Second Commit

---
+++ src/main/java/com/crushoncod/listeningserver/SpringBootListeningServerApplication.java
@@ -0,0 +1,13 @@
+package com.crushoncod.listeningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootListeningServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootListeningServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crushoncod/listeningserver/SpringBootListeningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crushoncod.listeningserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootListeningServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
